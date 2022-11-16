11a885599596b470fa5516c4e93ab997a2c3d95e
kirillsev1 <kirillprih47@gmail.com>
2022-11-16 14:40:38 +0300

    main file with class Ball

---
+++ main.py
@@ -0,0 +1,58 @@
+"""File with class Ball."""
+import math
+
+turnaround = 360
+
+
+class Ball:
+    """Class finds angle after moves."""
+
+    def __init__(self, speed=0, acceleration=0, radius=1, time=0):
+        """Method which initialize class Ball.
+
+        Args:
+            speed: int, float - speed of the ball.
+            acceleration: int, float - acceleration of the ball.
+            radius: int, float - radius of the ball.
+            time: int, float - rolling time.
+
+        Raises:
+            ValueError: Exception - if sides in allowed values.
+        """
+        self.speed = speed
+        self.acceleration = acceleration
+        self.radius = radius
+        self.time = time
+        if not self.check_instance():
+            raise ValueError
+        self.length_circle = 2 * self.radius * math.pi
+
+    def find_angle_speed(self):
+        """Method which finds deviation in degrees by speed.
+
+        Returns:
+            angle: float - angle in degrees.
+        """
+        path = self.speed * self.time
+        return round((path % self.length_circle) / self.length_circle * turnaround, 2)
+
+    def find_angle_acceleration(self):
+        """Method which finds deviation in degrees by acceleration.
+
+        Returns:
+            angle: float - angle in degrees.
+        """
+        path = self.acceleration * self.time ** 2 / 2
+        return round((path % self.length_circle) / self.length_circle * turnaround, 2)
+
+    def check_instance(self):
+        """Method which checks class parameters.
+
+        Returns:
+            flag: bool - if parameters is int and bigger than 0.
+        """
+        if isinstance(all([self.radius, self.acceleration, self.time, self.speed]), int):
+            if all([self.radius, self.acceleration, self.time, self.speed]):
+                return True
+        return False
+# print(Ball(5, 2, 1, 5).find_angle_acceleration())
